10c57b18c6d9b4f159727e2f8c55ea4531572e76
diokn2007 <148245937+diokn2007@users.noreply.github.com>
2023-12-08 09:58:41 +0700

    Add files via upload

--- System/Core/helpers/console.js
+++ System/Core/helpers/console.js
@@ -1,20 +1,20 @@
-import chalk from 'chalk';
+﻿import chalk from 'chalk';
 
 const logger = {
   info: (message) => {
-    console.log(chalk.bold.hex('#1d542f').bold('[INFO]» ')+message);
+    console.log(chalk.bold.hex('#1d542f').bold('[THÔNG TIN]» ') + message);
   },
   warn: (message) => {
-    console.log(chalk.bold.hex('#945f2b').bold('[WARNING]» ')+message);
+    console.log(chalk.bold.hex('#945f2b').bold('[CẢNH BÁO]» ') + message);
   },
   error: (message) => {
-    console.log(chalk.bold.hex('#942b2b').bold('[ERROR]» ')+message);
+    console.log(chalk.bold.hex('#942b2b').bold('[LỖI]» ') + message);
   },
   system: (message) => {
-    console.log(chalk.bold.hex('#f59e42').bold('[SYSTEM]» ')+message);
+    console.log(chalk.bold.hex('#f59e42').bold('[HỆ THỐNG]» ') + message);
   },
-  custom: (message,type,color = "\x1b[36m") => {
+  custom: (message, type, color = "\x1b[36m") => {
     console.log(`${color}[${type}]\x1b[0m ${message}`);
   }
-}
+};
 export default logger;
